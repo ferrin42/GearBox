a5d4bae1efa43e569876af4283cb1b3d96a68a2e
Jake Ferrin <jake.ferrin@gmail.com>
2015-04-23 21:07:45 -0600

    Initial commit

---
+++ GearBox/GearBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GearBox
+//
+//  Created by Jake Ferrin on 4/23/15.
+//  Copyright (c) 2015 Jake Ferrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GearBox/GearBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GearBox
+//
+//  Created by Jake Ferrin on 4/23/15.
+//  Copyright (c) 2015 Jake Ferrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
