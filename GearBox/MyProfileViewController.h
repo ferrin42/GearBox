b30d3e9ed5cae6332d28b3c76a42ca674cce714c
Jake Ferrin <jake.ferrin@gmail.com>
2015-04-30 12:54:41 -0600

    Set up empty tab bar controller

---
+++ GearBox/MyProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyProfileViewController.h
+//  GearBox
+//
+//  Created by Jake Ferrin on 4/30/15.
+//  Copyright (c) 2015 Jake Ferrin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyProfileViewController : UIViewController
+
+@end
